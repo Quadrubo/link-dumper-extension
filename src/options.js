6fd5fe77a0b8bcbeb0bb00f648daa7547bce109a
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2022-12-11 15:55:30 +0100

    auth working

--- src/options.js
+++ src/options.js
@@ -1,4 +1,7 @@
-import { SHA256, Base64 } from "crypto-js";
+// import { SHA256, Base64 } from "crypto-js";
+var CryptoJS = require("crypto-js");
+import axios from "axios";
+
 
 window.onload = function() {
     document.getElementById('reset').addEventListener('click', function() {
@@ -7,27 +10,81 @@ window.onload = function() {
         });
     });
 
-    document.getElementById('login').addEventListener('click', function() {
-        const state = createRandomString(40); 
-        const verifier = createRandomString(128);
-        const challenge = base64Url(SHA256(verifier));
+    document.getElementById('login').addEventListener('click', getAccessToken);
+}
 
-        const redirectURL = chrome.identity.getRedirectURL('redirect');
+function callback(redirectURL) {
+    const urlParams = new URLSearchParams(new URL(redirectURL).search);
 
-        console.log(redirectURL);
+    const state = window.localStorage.getItem('state');
+    const codeVerifier = window.localStorage.getItem('code_verifier');
 
-        chrome.identity.launchWebAuthFlow({
-            url: "http://localhost/oauth/authorize?client_id=8&redirect_uri=" + redirectURL + "&response_type=code&scope=*&state=" + state + "&code_challenge=" + challenge + "&code_challenge_method=S256",
-            interactive: true    
-        }, function(redirectURL) {
-            console.log(redirectURL);
-            if (chrome.runtime.lastError) {
-                console.error(chrome.runtime.lastError);
-            } else {
-                //
-            }
+    if (!(state.length > 0) || state !== urlParams.get('state')) {
+        return;
+    }
+
+    let params = {
+        grant_type: 'authorization_code',
+        client_id: 9,
+        redirect_uri: chrome.identity.getRedirectURL(),
+        code_verifier: codeVerifier,
+        code: urlParams.get('code'),
+    }
+
+    console.log(redirectURL);
+    console.log(params);
+
+    axios.post('http://localhost/oauth/token', params)
+        .then(resp => {
+            console.log(resp);
+            
+            localStorage.removeItem('state');
+            localStorage.removeItem('verifier');
+
+            const {token_type, expires_in, access_token, refresh_token} = resp.data;
+
+            chrome.storage.local.set({
+                "access_token": access_token,
+                "token_type": token_type,
+                "expires_in": expires_in,
+                "refresh_token": refresh_token,
+            });
         })
+        .catch(e => {
+            console.dir(e);
+        });
+}
+
+function authorize() {
+    const redirectURL = chrome.identity.getRedirectURL();
+    const clientID = 9;
+    
+    const state = createRandomString(40); 
+    window.localStorage.setItem('state', state);
+    
+    const verifier = createRandomString(128);
+    window.localStorage.setItem('code_verifier', verifier);
+    
+    const challenge = base64Url(CryptoJS.SHA256(verifier));
+    
+    let authURL = "http://localhost/oauth/authorize";
+
+    authURL += `?client_id=${clientID}`;
+    authURL += `&redirect_uri=${encodeURIComponent(redirectURL)}`;
+    authURL += `&response_type=code`;
+    authURL += `&scope=`;
+    authURL += `&state=${state}`;
+    authURL += `&code_challenge=${challenge}`;
+    authURL += `&code_challenge_method=S256`;
+
+    return chrome.identity.launchWebAuthFlow({
+        url: authURL,
+        interactive: true    
     });
+}
+
+function getAccessToken() {
+    return authorize().then(callback);
 }
 
 function createRandomString(num) {
@@ -35,7 +92,7 @@ function createRandomString(num) {
 }
 
 function base64Url(string) {
-    return string.toString(Base64)
+    return string.toString(CryptoJS.enc.Base64)
         .replace(/\+/g, '-')
         .replace(/\//g, '_')
         .replace(/=/g, '');
--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import './popup.css';
+import axios from "axios";
 
 (function () {
   // We will make use of Storage API to get and store `count` value
@@ -28,87 +28,124 @@ import './popup.css';
     },
   };
 
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
+  // function setupCounter(initialValue = 0) {
+  //   document.getElementById('counter').innerHTML = initialValue;
 
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
+  //   document.getElementById('incrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'INCREMENT',
+  //     });
+  //   });
 
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
+  //   document.getElementById('decrementBtn').addEventListener('click', () => {
+  //     updateCounter({
+  //       type: 'DECREMENT',
+  //     });
+  //   });
+  // }
 
-  function updateCounter({ type }) {
-    counterStorage.get((count) => {
-      let newCount;
+  // function updateCounter({ type }) {
+  //   counterStorage.get((count) => {
+  //     let newCount;
 
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
+  //     if (type === 'INCREMENT') {
+  //       newCount = count + 1;
+  //     } else if (type === 'DECREMENT') {
+  //       newCount = count - 1;
+  //     } else {
+  //       newCount = count;
+  //     }
 
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
+  //     counterStorage.set(newCount, () => {
+  //       document.getElementById('counter').innerHTML = newCount;
 
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-          const tab = tabs[0];
+  //       // Communicate with content script of
+  //       // active tab by sending a message
+  //       chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //         const tab = tabs[0];
 
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            (response) => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
+  //         chrome.tabs.sendMessage(
+  //           tab.id,
+  //           {
+  //             type: 'COUNT',
+  //             payload: {
+  //               count: newCount,
+  //             },
+  //           },
+  //           (response) => {
+  //             console.log('Current count value passed to contentScript file');
+  //           }
+  //         );
+  //       });
+  //     });
+  //   });
+  // }
 
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get((count) => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
+  // function restoreCounter() {
+  //   // Restore count value
+  //   counterStorage.get((count) => {
+  //     if (typeof count === 'undefined') {
+  //       // Set counter value as 0
+  //       counterStorage.set(0, () => {
+  //         setupCounter(0);
+  //       });
+  //     } else {
+  //       setupCounter(count);
+  //     }
+  //   });
+  // }
 
-  document.addEventListener('DOMContentLoaded', restoreCounter);
+  // document.addEventListener('DOMContentLoaded', restoreCounter);
+  document.addEventListener('DOMContentLoaded', setUserName);
 
   // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    (response) => {
-      console.log(response.message);
-    }
-  );
+  // chrome.runtime.sendMessage(
+  //   {
+  //     type: 'GREETINGS',
+  //     payload: {
+  //       message: 'Hello, my name is Pop. I am from Popup.',
+  //     },
+  //   },
+  //   (response) => {
+  //     console.log(response.message);
+  //   }
+  // );
+
+  async function setUserName() {
+    const name = await getUserName();
+    
+    document.getElementById('name').innerHTML = name;
+  }
+
+  async function readLocalStorage(key) {
+    return new Promise((resolve, reject) => {
+      chrome.storage.local.get([key], function (result) {
+        if (result[key] === undefined) {
+          reject();
+        } else {
+          resolve(result[key]);
+        }
+      });
+    }); 
+  }
+
+  async function getUserName() {
+    let access_token = await readLocalStorage("access_token");
+    let token_type = await readLocalStorage("token_type");
+
+    return new Promise((resolve, reject) => {
+      axios.get('http://localhost/api/user', {
+        headers: {
+          'Accept': 'application/json',
+          'Authorization': `${token_type} ${access_token}`,
+        }
+      }).then(response => {
+        resolve(response.data.name);
+      }).catch(error => {
+        reject(error);
+      });
+    });
+  }
 
   document.getElementById("options").addEventListener('click', function(e){
     chrome.tabs.create({url: `chrome-extension://${chrome.runtime.id}/options.html`}, function (tab) {
